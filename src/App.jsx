7d8ea1896f0b7fd5acd1a0927d324c08f35a782e
Mohd Zaid <zaid.ahmed.19nov@gmail.com>
2024-09-19 14:48:41 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+
+  useEffect(() => {
+    const newSocket = new WebSocket('ws://localhost:8080');
+    newSocket.onopen = () => {
+      console.log('Connection established');
+      newSocket.send('Hello Server!');
+      setSocket(newSocket)
+    }
+    newSocket.onmessage = (message) => {
+      console.log('Message received:', message.data);
+    }
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, [])
+
+  if(!socket)
+  {
+    return(
+    <div>
+      Loading...
+    </div> );
+  }
+
+  return (
+    <>
+      hi there
+    </>
+  )
+}
+
+export default App
